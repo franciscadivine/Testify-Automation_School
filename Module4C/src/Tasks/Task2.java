f873338a433f06048381925ea75dc7020136c611
Francisca Ovie <franciscadivine4@gmail.com>
2025-12-23 10:38:40 +0100

    Module4C tasks

---
+++ Module4C/src/Tasks/Task2.java
@@ -0,0 +1,33 @@
+package Tasks;
+
+public class Task2 {   public static void main(String[] args){
+//To print Byte
+    byte age = 127;
+    System.out.println (age);
+
+    //To print short
+    short amount = 32_678;
+    System.out.println(amount);
+
+    //To print long
+    long miles = -12789597788l;
+    System.out.println(miles);
+
+    //To print float
+    float measurement = 1200078.906f;
+    System.out.println(measurement);
+
+    //To print double
+    double report = 1209678544.890;
+    System.out.println(report);
+
+    //To print character
+    char option = 'A';
+    System.out.println(option);
+
+    //To print boolean
+    boolean  isTrue = false;
+    System.out.println(isTrue);
+
+}
+}
